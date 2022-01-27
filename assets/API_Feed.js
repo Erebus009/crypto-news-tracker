fac6440b580b588834d8f181d5f3fd207f7d2e58
Travis Puryear <travispuryear678@gmail.com>
2022-01-26 21:26:49 -0500

    Fix cause coinbase API changed to UUID.

--- assets/API_Feed.js
+++ assets/API_Feed.js
@@ -1,10 +1,53 @@
-const coinLIST = {BITCOIN: 1, ETHEREUM: 2, HEX: 5331, TETHER: 8, CARDANO: 9, BINANCE_COIN: 14, SOLANA: 68905, 
-TERRA: 62458, XRP: 3, POLKADOT: 71983, DOGECOIN: 20, USDC: 1760, UNISWAP: 72821, AVALANCHE: 70974, BINANCE: 14066, CHAINLINK: 59,
-LITECOIN: 7, ALGORAND: 14585, BITCOIN_CASH: 4, WRAPPED_BTC: 10607, COSMOS: 4966, POLYGON: 12606, AXIE_INFINITY: 76192,
-INTERNET_COMPUTER: 73728, PANCAKESWAP: 73268, VECHAIN: 19, TEZOS: 18, FTX_TOKEN: 62569, SHIBA_INU: 72724,
-DAI: 68589, STELLAR: 6, TRON: 11, ETHEREUM_CLASSIC: 16, FILECOIN: 74500, THETA_TOKEN: 96, COMPOUND_ETHER: 10825, MONERO: 10,
-OKB: 1524, EOS: 5, HEDERA_HASHGRAPH: 63100, ELROND: 72208, AAVE: 74883,  AMP: 77558, CELSIUS_NETWORK: 4303,
-LIDO_DAO_TOKEN: 78547, IOTA: 12, CONTENT_VALUE_NETWORK: 11170, KLAYTN: 63279, NEO: 15, ALL: 's'}
+const coinLIST = { BITCOIN: "Qwsogvtv82FCd",
+ ETHEREUM: "razxDUgYGNAdQ",
+ TETHER_USD: "HIVsRcGKkPFtW",
+ BINANCE_COIN: "WcwrkfNI4FUAe",
+ HEX: "9K7m6ufraZ6gh",
+ USDC: "aKzUVe4Hh_CON",
+ CARDANO: "qzawljRxB5bYu",
+ SOLANA: "zNZHO_Sjf",
+ XRP: "-l8Mn2pVlRs-p",
+ TERRA: "AaQUAs2Mc",
+ POLKADOT: "25W7FG7om",
+ DOGECOIN: "a91GCGd_u96cF",
+ BINANCE_USD: "vSo2fu9iE1s0Y",
+ AVALANCHE: "dvUj0CzDZ",
+ SHIBA_INU: "xz24e0BjL",
+ POLYGON: "uW2tk-ILY0ii",
+ WRAPPED_BTC: "x4WXHge-vvFY",
+ DAI: "MoTuySvg7",
+ COSMOS: "Knsels4_Ol-Ny",
+ LITECOIN: "D7B1x_ks7WhV5",
+ CRYPTO_CHAIN: "65PHZTpmE55b",
+ CHAINLINK: "VLqpJwogdhHNb",
+ NEAR_PROTOCOL: "DCrsaMv68",
+ UNISWAP: "_H5FVG9iW",
+ ALGORAND: "TpHE2IShQw-sJ",
+ TRON: "qUhEFk1I61atv",
+ OKB: "PDKcptVnzJTmN",
+ BITCOIN_CASH: "ZlZpzOJo43mIo",
+ FTX_TOKEN: "NfeOYfNcl",
+ DECENTRALAND: "tEf7-dnwV3BXS",
+ STELLAR: "f3iaFeCKEmkaZ",
+ HEDERA_HASHGRAPH: "jad286TjB",
+ FANTOM: "uIEWfMFnQo9K_",
+ LEO: "mqtUpyBxu8O8",
+ VECHAIN: "FEbS54wxo4oIl",
+ COMPOUND_ETHER: "p_GHkOeDNKw0",
+ THETA_TOKEN: "B42IRxNtoYmwK",
+ AXIE_INFINITY: "gpRKmM16k",
+ KLAYTN: "M9bj_WrX",
+ PANCAKESWAP: "ncYFcP709",
+ THE_SANDBOX: "pxtKbG5rg",
+ HELIUM: "rGDiacWtB",
+ ETHEREUM_CLASSIC: "hnfQfsYfeIGUQ",
+ ELROND: "omwkOTglq",
+ FRAX: "KfWtaeV1W",
+ MONERO: "3mVx2FX_iJFp5",
+ COMPOUND_DAI: "lT__vMO7l",
+ TEZOS: "fsIbGOEJWbzxG",
+ INCH_TOKEN: "lD9digIOk",
+ ALL: 's'}
 
 
 //this is the main object and constructor for the coin that is being searched from the API and used to build the elements of the we site
@@ -13,12 +56,12 @@ const coin = async (coinType, time = 0) => {
 
   //get the ID from the "coinList" array to match the format required by the API, can not search by just the name
   //bitcoin id = 1, Dogecoin = 20, etc.
-    let coinAPI = "https://coinranking1.p.rapidapi.com/coin/" + coinLIST[coinType]
+    let coinAPI = "https://coinranking1.p.rapidapi.com/coin/" + coinLIST[coinType] 
     
     //this checks to see if there is a time stamp passed and append it to the API url
     //the default is 0 if no param is passed to the function and the default search will be passed
     if (time != 0){
-      coinAPI += "/history/" + time;
+      coinAPI += "/history?timePeriod=" + time
     }
 
     //pass the dynamic coin URL to the fetch function to return the results
@@ -27,6 +70,7 @@ const coin = async (coinType, time = 0) => {
     //return the functions based on the param passed to the function
     //the default returns are based on time = 0, so no time param being used
     if (time === 0){
+      
       return ({
         all: () => coinDetails,
         name: () => coinDetails.data.coin.name,
@@ -34,11 +78,10 @@ const coin = async (coinType, time = 0) => {
         price: () => coinDetails.data.coin.price,
         icon_url: () => coinDetails.data.coin.iconUrl,
         change: () => coinDetails.data.coin.change,
-        volume: () => coinDetails.data.coin.volume,
+        volume: () => coinDetails.data.coin['24hVolume'],
         history: () => coinDetails.data.coin.history,
         rank: () => coinDetails.data.coin.rank,
         highest: () => coinDetails.data.coin.allTimeHigh.price,
-        name: () => coinDetails.data.coin.name,
         coinDesc:() => coinDetails.data.coin.description,
         link: () => coinDetails.data.coin.links[0].name,
         symbol: () => coinDetails.data.coin.symbol,
@@ -65,7 +108,7 @@ async function getCoin(url){
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-        "x-rapidapi-key": "4a5a441082mshcb1b714a2823090p1a255ajsnf13ed5558b31"
+        "x-rapidapi-key": "50e2e462f8msh7a5c1f25c65202ep1015a0jsn0a57f58b48d8"
       }
     })
     if(!response.ok){
